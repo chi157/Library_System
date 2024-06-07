0e1118e7ced0550d1816c88d52d015e6e32f0da9
Cynthia Chang <cynthia@CynthiadeMacBook-Air.local>
2024-06-07 10:36:42 +0800

    0607

--- src/main/java/com/example/demo/SpringBootJpaApplication.java
+++ src/main/java/com/example/demo/SpringBootJpaApplication.java
@@ -2,12 +2,13 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.format.FormatterRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 @Configuration
 public class SpringBootJpaApplication implements WebMvcConfigurer {
 
